92a2295c731bbd52cdcbff58dd9275b831c1c0d6
sinsincoccr <145324925+sinsincoccr@users.noreply.github.com>
2024-08-19 21:57:12 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 9.59 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String s) {
+        
+        
+        if(s.length() % 2 == 0){
+            return "" + s.charAt(s.length()/2-1) + s.charAt(s.length()/2); 
+        }else{
+            return "" + s.charAt(s.length()/2);
+        }
+        
+        
+        
+    }
+}
