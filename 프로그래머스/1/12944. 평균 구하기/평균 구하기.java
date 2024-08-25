c8db2f9d6df86c8c35c031118a976bb9580b4150
sinsincoccr <145324925+sinsincoccr@users.noreply.github.com>
2024-08-25 22:12:58 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int count =0;
+        for(int i=0; i< arr.length; i++){
+            answer += arr[i];
+            count = i;
+        }
+        
+        return answer / (count+1);
+    }
+}
